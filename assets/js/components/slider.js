1a351d7babf667c3029f53f8e85c7e08c1706449
Mayke <mayke_sargento@hotmail.com>
2017-08-08 13:49:36 -0300

    adding slider

---
+++ assets/js/components/slider.js
@@ -0,0 +1,12 @@
+(function () {
+  'use strict';
+
+  $('.slider .owl-carousel').owlCarousel({
+    loop: true,
+    nav: true,
+    dots: true,
+    items: 1,
+    navText: ['<i class="icon-arrow-left"/>', '<i class="icon-arrow-right"/>']
+  });
+
+})();
