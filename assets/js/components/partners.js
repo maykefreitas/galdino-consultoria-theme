64a6737b315aba3a3bab547a419f8fb564490553
Mayke <mayke_sargento@hotmail.com>
2017-08-11 10:05:52 -0300

    fixing parceiros page

--- assets/js/components/partners.js
+++ assets/js/components/partners.js
@@ -2,26 +2,31 @@
   'use strict';
 
   app.bindPartners = function () {
-    const $slider = $('.partners-slider');
+    if (window.innerWidth < 768) {
+      const $slider = $('.partners-slider');
 
-    $('.owl-carousel', $slider).owlCarousel({
-      loop: true,
-      nav: true,
-      dots: true,
-      margin: 30,
-      navText: ['<i class="icon-arrow-left"/>', '<i class="icon-arrow-right"/>'],
-      responsive: {
-        0: {
-          items: 1
-        },
-        480: {
-          items: 4
-        },
-        768: {
-          items: 6
+      $('.owl-carousel', $slider).owlCarousel({
+        loop: true,
+        nav: true,
+        dots: true,
+        margin: 30,
+        navText: ['<i class="icon-arrow-left"/>', '<i class="icon-arrow-right"/>'],
+        responsive: {
+          0: {
+            items: 1
+          },
+          480: {
+            items: 2
+          },
+          560: {
+            items: 3
+          },
+          690: {
+            items: 4
+          }
         }
-      }
-    });
+      });
+    }
   };
 
 })();
